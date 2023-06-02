e17c7bc54b316d0dc983e033ed4e336b580b933f
ebedoyaalzate <esteban.bedoya.contractor@bbva.com>
2023-06-02 08:45:30 -0500

    add styledEnginedProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import Layout from '@/components/Layout'
 import Store from '@/context/Store'
 import '@/styles/globals.scss'
 import type {AppProps} from 'next/app'
+import {StyledEngineProvider} from '@mui/material/styles'
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <Store>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Store>
+    <StyledEngineProvider injectFirst>
+      <Store>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Store>
+    </StyledEngineProvider>
   )
 }
